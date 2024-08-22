1d9ede2e9e45279eb5fc7123cb69a33b03f2b631
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2024-08-22 20:59:24 -0300

    add: productmapper

---
+++ SATest/app/mapper/product.mapper.ts
@@ -0,0 +1,32 @@
+import ProductEntity from "../entities/products.entity.js";
+import ProductModel from "../model/products.model.js";
+import { BaseMapper } from "./base.mapper.js";
+
+export class ProductMapper implements BaseMapper<ProductEntity, ProductModel> {
+
+
+  toModel(entity: ProductEntity): ProductModel {
+    return new ProductModel({
+      id: entity.id,
+      name: entity.name,
+      value: entity.value,
+      quantity: entity.quantity,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+
+    })
+  }
+  toEntity(model: ProductModel): ProductEntity {
+    const entity = new ProductEntity()
+
+    entity.id = model.Id
+    entity.name = model.name
+    entity.quantity = model.quantity
+    entity.value = model.value
+    entity.createdAt = model._createdAt
+    entity.updatedAt = model._updatedAt
+
+    return entity
+  }
+
+}
