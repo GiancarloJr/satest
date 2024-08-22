77b619e27f7b54f6cb26cd65e8d9cfa5374223da
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2024-08-21 23:49:05 -0300

    add routes

--- SATest/start/routes.ts
+++ SATest/start/routes.ts
@@ -7,10 +7,11 @@
 |
 */
 
+import ProductsController from '#controllers/products_controller'
+import GroupProductsController from '../app/controllers/group_products_controller.js'
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+
+router.resource('product-group', GroupProductsController).apiOnly()
+
+router.resource('/product-group/:idgp/product', ProductsController).apiOnly()
